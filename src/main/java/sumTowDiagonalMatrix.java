a5ce6b2c85c870a7c3751d1bc2d050b869150bc6
admin1 <hai2108hatayqo@gmail.com>
2019-03-28 00:24:47 +0700

    add file

---
+++ src/main/java/sumTowDiagonalMatrix.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class sumTowDiagonalMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+        do {
+            System.out.println("Enter a size matrix:");
+            size = scanner.nextInt();
+            if (size > 15){
+                System.out.println("Size should NOT exceed 15!!!");
+            }
+        }while (size >15);
+        //Enter value for element
+        int [][] matrix = new int[size][size];
+        for (int i=0; i<size; i++){
+            for (int j=0; j<size; j++){
+                System.out.println("Element row "+ (i+1) + ", col "+ (j+1) + " is: ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        //Show Matrix
+        System.out.println("Matrix is: ");
+        for (int i=0; i<size; i++){
+            System.out.println(" ");
+            for (int j=0; j<size;j++){
+                System.out.print(matrix[i][j] + "\t");
+            }
+        }
+        System.out.println(" ");
+        //Sum 2 diagonal
+        int sum = 0;
+        for(int i=0; i<size;i++){
+            sum += matrix[i][i] + matrix[i][size-1-i];
+        }
+        System.out.println("Sum Numbers in Tow Diagonal os Matrix is: "+sum);
+    }
+}
